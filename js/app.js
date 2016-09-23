d4d53b1fbb374b7026036c7e457b3c54d57b2f47
mel1bt <mberrocal.tipte@gmail.com>
2016-09-22 20:55:55 -0500

    cuarta actualizaacion

--- js/app.js
+++ js/app.js
@@ -14,27 +14,58 @@ window.addEventListener("load",function(){
     var inpForm = document.getElementById("inpForm");
     btnForm.addEventListener("click", function(e){
         tarjeta();
-        spanForm.style.display = "block";
-        formList.style.display = "none";
         
     });
     function tarjeta(){
         var newTarjeta = document.createElement("div");
-        /*var newTituTarj = document.createElement("h5");
-        var newTextaTarj = document.createElement("textarea"); */
-        var node = document.createTextNode(inpForm.value);
-        newTarjeta.appendChild(node);
-        /*
-        newTituTarj.appendChild(node);
+        var newTituTarj = document.createElement("h5");
+        var newlinkTarj = document.createElement("a");
+        newlinkTarj.href="#";
+        
+        var nodeTitulo = document.createTextNode(inpForm.value);
+        var nodeNuevo = document.createTextNode("Añadir una tarjeta");
+        newTituTarj.appendChild(nodeTitulo);
+        newlinkTarj.appendChild(nodeNuevo);
         newTarjeta.appendChild(newTituTarj);
-        newTarjeta.appendChild(newTextaTarj);*/
-        newTarjeta.classList.add("tarjeta");
+        newTarjeta.appendChild(newlinkTarj);
+        newTarjeta.classList.add("tarjeta"); 
         spanForm.parentNode.insertBefore(newTarjeta,spanForm.previousSibling);
-        //newTarjeta.style.cssFloat = "left";
-    }
-    
-    
-    
+        newTarjeta.style.cssFloat = "left";
+        spanForm.style.display = "inline-block";
+        formList.style.display = "none";
+        
+        newlinkTarj.addEventListener("click", function(){
+            var newInpTarj = document.createElement("textarea");
+            var newButtonS = document.createElement("button");
+            newButtonS.classList.add("btn");
+            newButtonS.classList.add("btn-success");
+             newButtonS.classList.add("botAni");
+            var newnodo = document.createTextNode("Añadir");
+            newButtonS.appendChild(newnodo);
+            newTarjeta.appendChild(newInpTarj);
+            newTarjeta.appendChild(newButtonS);
+            newTarjeta.insertBefore(newInpTarj, newTituTarj.nextSibling);
+            newTarjeta.insertBefore(newButtonS, newInpTarj.nextSibling);
+            newlinkTarj.style.display="none";
+            /*
+            newButtonS.addEventListener("click", function(){
+                newInpTarj.style.display = "none";
+                newButtonS.style.display = "none";
+                var textareaNew = document.createElement("textarea");
+                var nodoAreaNew = document.createTextNode(newInpTarj.value);
+                textareaNew.classList.add("subtarjeta");
+                textareaNew.appendChild(nodoAreaNew);
+                //newTarjeta.style.text-align = "center";
+                newTarjeta.appendChild(textareaNew);
+                newTarjeta.insertBefore(textareaNew, newTituTarj.nextSibling);
+                newlinkTarj.style.display="block";
+            });*/
+            
+        });
+        
+        
+        
+    }    
     
     
 });
