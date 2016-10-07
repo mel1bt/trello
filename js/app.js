c11b80707000c9e0fba2940ff04315be9302203c
mel1bt <mberrocal.tipte@gmail.com>
2016-10-06 21:25:14 -0500

    Optimizando codigo

--- js/app.js
+++ js/app.js
@@ -1,85 +1,94 @@
-window.addEventListener("load",function(){
-    var $a = jQuery.noConflict();//PARA EVITAR CONFLICTOS CON EL JQUERY
     var spanForm = document.getElementById('spanForm');
     var formList = document.getElementById('formList1');
     var areaTrabajo = document.getElementById('areaTrabajo');
-    var num =0;
-    formList.style.display = "none";
-    spanForm.addEventListener("click",function(e){
-		e.preventDefault();
-        formList.style.display = "block";
-        spanForm.style.display = "none";
-        formList.style.display="inline-block";
-        inpForm.value = "";
-        inpForm.focus();
-	});
-    //para la version 2
     var btnForm = document.getElementById("btnForm");
     var inpForm = document.getElementById("inpForm");
-    btnForm.addEventListener("click", function(e){
-        tarjeta();
-        aumentar();
-        
-    });
+    var num = 0;
 
-    function aumentar(){
-        var result = areaTrabajo.style.width;
-        result = Number(result.split("px",1));
-        result += 300;
-        areaTrabajo.style.width = result + "px";
-    }
+window.addEventListener("load", cargarPagina);
+    
+function cargarPagina(e){
+    e.preventDefault();
+    formList.style.display = "none";
+    spanForm.addEventListener("click", crearFormListaNueva);
+    btnForm.addEventListener("click", crearNuevaTarj);
+};
 
-        function tarjeta(){
-        var newTarjeta = document.createElement("div");
-        var newTituTarj = document.createElement("h5");
-        var newlinkTarj = document.createElement("a");
-        newlinkTarj.href="#";
-        
-        var nodeTitulo = document.createTextNode(inpForm.value);
-        var nodeNuevo = document.createTextNode("Añadir una tarjeta");
-        newTituTarj.appendChild(nodeTitulo);
-        newlinkTarj.appendChild(nodeNuevo);
-        newTarjeta.appendChild(newTituTarj);
-        newTarjeta.appendChild(newlinkTarj);
-        newTarjeta.classList.add("tarjeta"); 
-        spanForm.parentNode.insertBefore(newTarjeta,spanForm.previousSibling);
-        newTarjeta.style.cssFloat = "left";
-        spanForm.style.display = "inline-block";
-        formList.style.display = "none";
-        
-        newlinkTarj.addEventListener("click", function(){
-            var newInpTarj = document.createElement("textarea");
-            var newButtonS = document.createElement("button");
-            newInpTarj.classList.add("subtarjeta1");
-            newButtonS.setAttribute("type","button");
-            newButtonS.classList.add("btn");
-            newButtonS.classList.add("btn-success");
-             newButtonS.classList.add("botAni");
-            var newnodo = document.createTextNode("Añadir");
-            newButtonS.appendChild(newnodo);
-            newTarjeta.appendChild(newInpTarj);
-            newTarjeta.appendChild(newButtonS);
-            newTarjeta.insertBefore(newInpTarj, newTituTarj.nextSibling);
-            newTarjeta.insertBefore(newButtonS, newInpTarj.nextSibling);
-            newlinkTarj.style.display="none";
-            newInpTarj.addEventListener("keyup", function(){
-            var el = this;
-            el.style.cssText = 'padding:0; height:' + el.scrollHeight + 'px';
-            });
+function crearFormListaNueva(e){
+    e.preventDefault();
+    formList.classList.add("block","inline-block");
+    spanForm.style.display = "none";
+    formList.style.display="";
+    inpForm.value = "";
+    inpForm.focus();
+};
 
-            newButtonS.addEventListener("click", function(){
-                newInpTarj.style.display = "none";
-                newButtonS.style.display = "none";
-                var textareaNew = document.createElement("span");
-                var nodoAreaNew = document.createTextNode(newInpTarj.value);
-                //textareaNew.style.box-sizing = "content-box";
-                textareaNew.classList.add("subtarjeta");
-                textareaNew.appendChild(nodoAreaNew);
-                //newTarjeta.style.text-align = "center";
-                newTarjeta.appendChild(textareaNew);
-                newTarjeta.insertBefore(textareaNew, newTituTarj.previousSibling);
-                newlinkTarj.style.display="block";
-            }); 
-        });       
-    }    
-});
+function crearNuevaTarj(e){
+    e.preventDefault();
+    lista();
+    aumentar();
+}
+
+function aumentar(){
+    var result = areaTrabajo.style.width;
+    result = Number(result.split("px",1));
+    result += 300;
+    areaTrabajo.style.width = result + "px";
+}
+var newlinkTarj;
+function lista(){
+    
+    var newTarjeta = document.createElement("div");
+    var newTituTarj = document.createElement("b");
+    newTituTarj.classList.add("titulo");
+    newTituTarj.textContent = inpForm.value;
+    newlinkTarj = document.createElement("a");
+    newlinkTarj.innerHTML= "Añadir una tarjeta";
+    newlinkTarj.href="#";
+        
+    newTarjeta.appendChild(newTituTarj);
+    newTarjeta.appendChild(newlinkTarj);
+    newTarjeta.classList.add("tarjeta"); 
+    
+    spanForm.parentNode.insertBefore(newTarjeta,spanForm.previousSibling);
+    newTarjeta.style.cssFloat = "left";
+    spanForm.style.display = "inline-block";
+    formList.style.display = "none";
+    
+    newlinkTarj.addEventListener("click", formNuevaTarjeta);
+};
+
+function formNuevaTarjeta(e){
+    e.preventDefault();
+    newlinkTarj.style.display="none";
+    var newDiv = document.createElement("div");
+    var newInpTarj = document.createElement("textarea");
+    var newButtonS = document.createElement("button");
+    newInpTarj.classList.add("subtarjeta1");
+    newButtonS.classList.add("center", "btn", "btn-success", "botAni"); 
+    newButtonS.innerHTML = "Añadir";
+    newDiv.appendChild(newInpTarj);
+    newDiv.appendChild(newButtonS);
+    this.parentNode.insertBefore(newDiv, this);
+    newInpTarj.addEventListener("keyup", validacionTexto);
+    newButtonS.type="button"    ;
+    newButtonS.addEventListener("click", guardarTarjeta); 
+};
+
+function validacionTexto(){
+    var el = this;
+    el.style.cssText = 'padding:0; height:' + el.scrollHeight + 'px';
+};
+
+function guardarTarjeta(e){
+    e.preventDefault();
+    console.log(this);
+    this.previousSibling.style.display = "none";
+    this.style.display = "none";
+    var textareaNew = document.createElement("span");
+    textareaNew.classList.add("subtarjeta");
+    textareaNew.innerHTML = this.previousSibling.value;
+    this.parentElement.appendChild(textareaNew);
+    newlinkTarj.style.display="block";
+};
+    
